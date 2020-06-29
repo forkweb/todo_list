8ce183025a2c15cefbf9a35e062817f52c9075e4
fork_web <leshadgrad@mail.ru>
2020-06-30 00:12:00 +0400

    start

---
+++ src/components/TodoList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="todo-list">
+
+    <h1>Список заметок</h1>
+
+    <div class="todo-list__item">
+      <div class="todo-list__item-title">
+        Заголовок
+      </div>
+
+      <div class="todo-list__item-check">
+        <input type="checkbox" checked>
+        <input type="checkbox">
+      </div>
+      
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
